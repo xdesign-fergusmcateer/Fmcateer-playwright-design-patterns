72b938d52b1b58979c1f865ab20efe0460f22848
fmca <fhmcateer@hotmail.co.uk>
2026-03-18 16:43:34 +0000

    Added notes to the POM

--- page_objects/login-page.pom.ts
+++ page_objects/login-page.pom.ts
@@ -18,5 +18,36 @@ export class LoginPage {
 
     await this.signInButtonLocator.click();
     }
+//This is a login method, it's called an 'action'. It will allow us to perform the login process across multiple tests
+
 
 }
+
+//Note: avoid mixing business logic with your POM. I.e:
+//if (userType === 'admin') {
+//Logic inside POM - avoid this
+//}
+// keep your POM focused purely on the mechanics of INTERACTING with the PAGE
+
+//Avoid adding complex logic to your POM
+//i.e. some kind of counter function? or:
+//for (let i = 0; i < attempts; i++){
+//}
+
+//Each POM should represent a single page or component
+//Do not include multiple pages or components in one POM - create more POMs!
+
+//Don't hardcode values, like URLs, credentials or element identifiers
+//User parameters, constants or config files
+
+//Review and update your POMs as the application evolves
+
+//Don't tie your POM directly to URLs
+//POMs should focus on page's structure and behaviour, not specific URLs
+//Consider creating POMs that can be extended or composed into larger POMs
+
+//Avoid creating sugar methods in POMs
+//these are methods that obscure test actions
+//keep methods straightforward and reflective of real user interactions
+
+//Ensure locators in your POM are public
